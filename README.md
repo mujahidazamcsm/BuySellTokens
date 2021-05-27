edb4e569e6429389fcd7eb7a97abf3077eae5f15
PhatJay <jj@indie.org>
2021-05-26 22:28:50 -0400

    Hello Nurse!

---
+++ README.md
@@ -0,0 +1,9 @@
+# Buy & Sell Tokens using Ethers.js and NodeJS
+
+# Buy a token using BNB
+
+1. Clone repository
+1. Run npm i
+1. Edit env.js
+1. node buy.js
+1. profit
---
+++ buy.js
@@ -0,0 +1,67 @@
+"use strict";
+
+const Env = require("./env.json");
+Object.assign(process.env, Env);
+
+const ethers = require("ethers");
+
+const purchaseToken = process.env.PURCHASE_TOKEN;
+const purchaseAmount = Ethers.utils.parseUnits(
+  process.env.PURCHASE_AMOUNT,
+  "ether"
+); // buy in BNB
+
+const wbnb = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
+const pcs = "0x10ED43C718714eb63d5aA57B78B54704E256024E";
+const slippage = "1";
+
+const provider = new ethers.providers.WebSocketProvider(
+  process.env.BSC_NODE_WSS
+);
+const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC);
+const account = wallet.connect(provider);
+
+const router = new ethers.Contract(
+  pcs,
+  [
+    "function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)",
+    "function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline) external payable returns (uint[] memory amounts)",
+    "function swapExactETHForTokensSupportingFeeOnTransferTokens(uint amountOutMin, address[] calldata path, address to, uint deadline) external payable returns (uint[] memory amounts)",
+  ],
+  account
+);
+
+async function buy() {
+  const amounts = await router.getAmountsOut(purchaseAmount, [
+    wbnb,
+    purchaseToken,
+  ]);
+  const amountOutMin = amounts[1].sub(amounts[1].div(slippage));
+
+  console.log(`
+    Buying Token
+    =================
+    tokenIn: ${purchaseAmount.toString()} ${wbnb} (WBNB)
+    tokenOut: ${amountOutMin.toString()} ${purchaseToken} 
+  `);
+
+  // For "taxed" coins - swap out next line
+  //const txt = await router.swapExactETHForTokensSupportingFeeOnTransferTokens(
+  const tx = await router.swapExactETHForTokens(
+    amountOutMin,
+    [wbnb, purchaseToken],
+    process.env.RECIPIENT,
+    Date.now() + 1000 * 60 * 10, //10 minutes
+    {
+      value: purchaseAmount,
+      gasLimit: 345684,
+      gasPrice: ethers.utils.parseUnits("6", "gwei"),
+    }
+  );
+
+  const receipt = await tx.wait();
+  console.log("Transaction receipt");
+  console.log(receipt);
+  process.exit();
+}
+buy();
